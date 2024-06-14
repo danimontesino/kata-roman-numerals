0c35a1d0591442da9f0696c97ffba722a7b7be1e
Jose Barrera <jose@acidtango.com>
2024-06-14 10:45:38 -0400

    green: base case X, L, C, D, M

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,21 @@ export function numeralsToRoman(num: number): string {
   if (num === 5) {
     return 'V'
   }
+  if (num === 10) {
+    return 'X'
+  }
+  if (num === 50) {
+    return 'L'
+  }
+  if (num === 100) {
+    return 'C'
+  }
+  if (num === 500) {
+    return 'D'
+  }
+  if (num === 1000) {
+    return 'M'
+  }
   const charToRepeat = 'I'
   return charToRepeat.repeat(num);
 }
